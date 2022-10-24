0fa3223d2d9402b8464566b22ce1439cc6e9fb89
yanmiao99 <503084944@qq.com>
2022-10-24 19:19:51 +0800

    init : 大屏适配方案

--- README.md
+++ README.md
@@ -9,11 +9,5 @@ npm install
 ```
 npm run serve
 ```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+### 说明
+>只要把页面放在这个组件中，就能实现跟大厂们类似的效果。这种方式下不管屏幕有多大，分辨率有多高，只要屏幕的比例跟你定的比例一致，都能呈现出完美效果。而且开发过程中，样式的单位也可以直接用px，省去了转换的烦恼~~~
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
+    <scaleBox>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+      <router-view/>
+    </scaleBox>
   </div>
 </template>
+
+<script>
+import scaleBox from "@/components/ScaleBox";
+
+export default {
+  components: {
+    scaleBox
+  }
+
+}
+</script>
+
 
 <style lang="less">
 #app {
